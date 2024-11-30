928e2db831ec36ff03559a8a0562b97c27c61594
Juan Fabro <69061114+fabrojuan@users.noreply.github.com>
2024-11-30 19:42:04 -0300

    se agrega documentacion con OpenApi

--- src/main/kotlin/com/challenges/tripoptimizerapi/controller/PathController.kt
+++ src/main/kotlin/com/challenges/tripoptimizerapi/controller/PathController.kt
@@ -3,20 +3,25 @@ package com.challenges.tripoptimizerapi.controller
 import com.challenges.tripoptimizerapi.dto.PathCreationDto
 import com.challenges.tripoptimizerapi.dto.resource.PathResource
 import com.challenges.tripoptimizerapi.service.IPathService
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/paths")
+@Tag(name = "Path API", description = "Manage path resources in the app")
 class PathController(val pathService: IPathService) {
 
     @PutMapping("/{path_id}")
+    @Operation(summary = "Create Path", description = "Create a new path with its origin, destination and cost")
     fun createStation(@PathVariable(name="path_id") pathId: Long,
                       @RequestBody pathCreationData: PathCreationDto): ResponseEntity<PathResource> {
         return ResponseEntity.ok(pathService.savePath(pathId, pathCreationData))
     }
 
     @GetMapping("/{source_id}/{destination_id}")
+    @Operation(summary = "Get optimal Path", description = "Retrieve an optimal path between an origin and a destination")
     fun getOptimalPath(@PathVariable(name = "source_id") sourceId: Long,
                        @PathVariable(name = "destination_id") destinationId: Long) {
 
--- src/main/kotlin/com/challenges/tripoptimizerapi/controller/StationController.kt
+++ src/main/kotlin/com/challenges/tripoptimizerapi/controller/StationController.kt
@@ -3,6 +3,8 @@ package com.challenges.tripoptimizerapi.controller
 import com.challenges.tripoptimizerapi.dto.StationCreationDto
 import com.challenges.tripoptimizerapi.dto.resource.StationResource
 import com.challenges.tripoptimizerapi.service.IStationService
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PutMapping
@@ -12,9 +14,11 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/stations")
+@Tag(name = "Station API", description = "Manage station resources in the app")
 class StationController(val stationService: IStationService) {
 
     @PutMapping("/{station_id}")
+    @Operation(summary = "Create Station", description = "Create a new station with its name")
     fun createStation(@PathVariable(name = "station_id") stationId: Long,
                       @RequestBody stationCrationData: StationCreationDto): ResponseEntity<StationResource> {
         return ResponseEntity.ok(stationService.saveStation(stationId, stationCrationData))
